8eb0385fb2a08a0208af2b59362a14581af746f4
Arius23 <ariellanci@gmail.com>
2025-09-17 03:06:21 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: '/Audiolibros-Horacio-Lanci/',
   plugins: [react()],
   resolve: {
     alias: {
@@ -10,4 +11,5 @@ export default defineConfig({
     },
   },
 })
+
 
