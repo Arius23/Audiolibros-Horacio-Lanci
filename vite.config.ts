bdda5946218dbbbc14d67e8f8b8a6494eb2d5022
Arius23 <ariellanci@gmail.com>
2025-09-20 15:48:28 -0300

    build: base para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Audiolibros-Horacio-Lanci/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+plugins: [react()],
+base: '/Audiolibros-Horacio-Lanci/',
 })
 
 
